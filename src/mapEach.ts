c72e160ff3d31b5db802adb4107f49b54c6e61cc
Oliver Deng <oliverdeng1005@hotmail.com>
2024-06-06 21:03:27 +1200

    add mapBatch method

--- src/mapEach.ts
+++ src/mapEach.ts
@@ -1,4 +1,4 @@
-export async function mapEach<T, S>(items: T[], cb: (item: T, i: number) => S): Promise<S[]> {
+export async function mapEach<T, S>(items: T[], cb: (item: T, i: number) => S | Promise<S>): Promise<S[]> {
   const result: S[] = [];
 
   for (let i = 0; i < items.length; i++) {
